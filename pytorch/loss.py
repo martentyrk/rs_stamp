fd366de28b96df85a1e581d29a81544d9edfddac
Marten   Turk <lcur2471@test3.irc.sara.nl.irc.sara.nl>
2023-06-06 15:17:12 +0200

    init

---
+++ pytorch/loss.py
@@ -0,0 +1,47 @@
+# @Time   : 2020/6/26
+# @Author : Shanlei Mu
+# @Email  : slmu@ruc.edu.cn
+
+# UPDATE:
+# @Time   : 2020/8/7, 2021/12/22
+# @Author : Shanlei Mu, Gaowei Zhang
+# @Email  : slmu@ruc.edu.cn, 1462034631@qq.com
+
+
+"""
+recbole.model.loss
+#######################
+Common Loss in recommender system
+"""
+
+import torch
+import torch.nn as nn
+
+
+class BPRLoss(nn.Module):
+    """BPRLoss, based on Bayesian Personalized Ranking
+
+    Args:
+        - gamma(float): Small value to avoid division by zero
+
+    Shape:
+        - Pos_score: (N)
+        - Neg_score: (N), same shape as the Pos_score
+        - Output: scalar.
+
+    Examples::
+
+        >>> loss = BPRLoss()
+        >>> pos_score = torch.randn(3, requires_grad=True)
+        >>> neg_score = torch.randn(3, requires_grad=True)
+        >>> output = loss(pos_score, neg_score)
+        >>> output.backward()
+    """
+
+    def __init__(self, gamma=1e-10):
+        super(BPRLoss, self).__init__()
+        self.gamma = gamma
+
+    def forward(self, pos_score, neg_score):
+        loss = -torch.log(self.gamma + torch.sigmoid(pos_score - neg_score)).mean()
+        return loss
