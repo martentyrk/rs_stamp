a2574232123a6ac5fd155f0bd637a0c00ab187c4
Farrukh Baratov <farrukhsbaratov@gmail.com>
2023-06-22 00:56:59 +0200

    added linux tf-env, repeat ratio, stamp_cikm

--- tensorflow/model/STAMP_cikm.py
+++ tensorflow/model/STAMP_cikm.py
@@ -12,7 +12,8 @@ from util.Pooler import pooler
 from basic_layer.FwNn3AttLayer import FwNnAttLayer
 from util.FileDumpLoad import dump_file
 from util.save_results import save_results
-from util.RepeatRatio import repeat_ratio
+from util.RepeatRatio import repeat_ratio_sample
+from tqdm import tqdm
 
 class Seq2SeqAttNN(NN):
     """
@@ -194,7 +195,9 @@ class Seq2SeqAttNN(NN):
                 class_num= self.n_items,
                 random=True
             )
-            while bt.has_next():    # batch round.
+            for _ in tqdm(range(len(bt.rand_idx))):
+                if not bt.has_next():
+                    break
                 # get this batch data
                 batch_data = bt.next_batch()
                 # build the feed_dict
@@ -313,7 +316,7 @@ class Seq2SeqAttNN(NN):
                 return
             print('Epoch{}\tloss: {:.6f}'.format(epoch, avgc))
             if test_data != None:
-                recall, mrr = self.test(sess, test_data)
+                recall, mrr, repeat = self.test(sess, test_data)
                 print(recall, mrr)
                 if max_recall < recall:
                     max_recall = recall
@@ -321,18 +324,19 @@ class Seq2SeqAttNN(NN):
                     test_data.update_best()
                     if max_recall > threshold_acc:
                         self.save_model(sess, self.config, saver)
-                print ("                   max_recall: " + str(max_recall)+" max_mrr: "+str(max_mrr))
+                print ("                   max_recall: " + str(max_recall)+" max_mrr: "+str(max_mrr) + " repeat: " + str(repeat))
                 test_data.flush()
         if self.is_print:
             save_results(self.config, max_recall, max_mrr, split='val')
             TIPrint(test_data.samples, self.config,
-                    {'recall': max_recall, 'mrr': max_mrr}, True)
+                    {'recall': max_recall, 'mrr': max_mrr, 'repeat': repeat}, True)
 
     def test(self,sess,test_data):
 
         # calculate the acc
         print('Measuring Recall@{}, MRR@{}, and Repeat@{}'.format(self.cut_off, self.cut_off, self.cut_off))
-        mrr, recall, repeat = [], [], []
+        mrr, recall = [], []
+        repeat = []
         c_loss =[]
         batch = 0
         bt = batcher(
@@ -340,7 +344,10 @@ class Seq2SeqAttNN(NN):
             class_num = self.n_items,
             random = False
         )
-        while bt.has_next():    # batch round.
+        for _ in tqdm(range(len(bt.rand_idx))):
+            if not bt.has_next():
+                break
+            #while bt.has_next():    # batch round.
             # get this batch data
             batch_data = bt.next_batch()
             # build the feed_dict
@@ -454,7 +461,7 @@ class Seq2SeqAttNN(NN):
                         feed_dict=feed_dict
                     )
                     t_r, t_m, ranks = cau_recall_mrr_org(preds, batch_out, cutoff=self.cut_off)
-                    t_repeat = repeat_ratio(batch_in, preds, cutoff=self.cut_off)
+                    t_repeat = repeat_ratio_sample(batch_in, preds, k=self.cut_off)
                     test_data.pack_ext_matrix('alpha', alpha, tmp_batch_ids)
                     test_data.pack_preds(ranks, tmp_batch_ids)
                     c_loss += list(loss)
@@ -464,6 +471,6 @@ class Seq2SeqAttNN(NN):
                     repeat += t_repeat
         r, m =cau_samples_recall_mrr(test_data.samples,self.cut_off)
         print (r,m)
-        print(f"Mean Repeat ratio: {round(sum(repeat) / len(repeat), 3)}")
+        #print(f"Mean Repeat ratio: {sum_repeat / num_items}")
         print (np.mean(c_loss))
-        return  np.mean(recall), np.mean(mrr)#, np.mean(repeat)
+        return  np.mean(recall), np.mean(mrr), np.mean(repeat)
--- tensorflow/util/RepeatRatio.py
+++ tensorflow/util/RepeatRatio.py
@@ -7,8 +7,30 @@ def repeat_ratio(samples, preds, k=20):
     for b_in, b_out in zip(samples, preds):
         # go through samples in each batch
         for s_in, s_out in zip(b_in, b_out):
-            topk = np.argsort(s_out).flip()[:k] # get topk
+            topk = np.flip(np.argsort(s_out))[:k] # get topk
             ratio = np.intersect1d(s_in, topk) / k
             ratios.append(ratio)
 
     return ratios
+
+def repeat_ratio_sample(samples, preds, k=20):
+    ratios = []
+    
+    # go through each sample
+    for s_in, s_out in zip(samples, preds):
+        topk = np.flip(np.argsort(s_out))[:k] # get topk
+        ratio = np.intersect1d(s_in, topk).shape[0] / k
+        ratios.append(ratio)
+
+    return ratios
+
+
+def test0():
+    sample = np.array([[0,1,5,6,7,8,9]])
+    pred = np.array([[1,1,-1,-1,-1]])
+    ratio = repeat_ratio_sample(sample, pred, k=5)[0]
+    assert ratio == 0.4, f"Ratio should be 0.4, not {ratio}!"
+
+if __name__ == "__main__":
+    # run a unit test
+    test0()
