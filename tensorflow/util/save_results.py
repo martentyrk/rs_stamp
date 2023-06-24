8e918a7d049bec30a846affe5158bf1845200f2f
uvandre <57217247+uvandre@users.noreply.github.com>
2023-06-24 15:50:01 +0200

    better saving

--- tensorflow/util/save_results.py
+++ tensorflow/util/save_results.py
@@ -6,22 +6,25 @@ import os
 def save_results(config, recall, mrr, split='test'):
     suf = time.strftime("%Y%m%d%H%M", time.localtime())[4:]
     path = f"output/{split}_results.csv"
-    
-    results = {'time':time.strftime("%Y%m%d%H%M", time.localtime()), 'model': config['model'], 'dataset': config['dataset'], '@k':config['cut_off'], 'kfolds':config['k_folds'], 'recall':recall, 'mrr':mrr}
+    results_dict = {'time':time.strftime("%Y%m%d%H%M", time.localtime()), 'model': config['model'], 'dataset': config['dataset'], '@k':config['cut_off'], 'kfolds':config['k_folds'], 'recall':recall, 'mrr':mrr}
+    results = pd.DataFrame(results_dict, index=[0])
     if os.path.exists(path):
-        results_df = pd.read_csv(path)
+        results_df = pd.read_csv(path, index_col=0)
     else:
         results_df = pd.DataFrame()
-    results_df = results_df.append(results, ignore_index=True)
+    results_df = results_df.append(results, ignore_index=False)
     results_df.to_csv(path)
+    return results_dict
 
 
 def save_repeat_ratio_results(config, repeat_ratio):
     path = f"output/repeat_ratio_results.csv"
-    results = {'time':time.strftime("%Y%m%d%H%M", time.localtime()), 'model': config['model'], 'dataset': config['dataset'], '@k':config['cut_off'], 'kfolds':config['k_folds'], 'repeat_ratio':repeat_ratio}
+    results_dict = {'time':time.strftime("%Y%m%d%H%M", time.localtime()), 'model': config['model'], 'dataset': config['dataset'], '@k':config['cut_off'], 'kfolds':config['k_folds'], 'repeat_ratio':repeat_ratio}
+    results = pd.DataFrame(results_dict, index=[0])
     if os.path.exists(path):
-        results_df = pd.read_csv(path)
+        results_df = pd.read_csv(path, index_col=0)
     else:
         results_df = pd.DataFrame()
-    results_df = results_df.append(results, ignore_index=True)
+    results_df = results_df.append(results, ignore_index=False)
     results_df.to_csv(path)
+    return results_dict
